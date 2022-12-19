fba564b63af64a29a049210ca7b8aeb5362a8248
Zsomi4 <k72302872172@ktch.hu>
2022-12-19 09:31:28 +0100

    Fejléc megírása.

--- src/App.java
+++ src/App.java
@@ -1,3 +1,12 @@
+/*
+* File: App.java
+* Author: Gubis Zsombor Dániel
+* Copyright: 2022, Gubis Zsombor Dániel
+* Group: Szoft_II_N
+* Date: 2022-12-19
+* Github: https://github.com/Zsomi4/
+* Licenc: GNU GPL
+*/
 import controllers.MainController;
 
 public class App {
--- src/views/VolumePanel.java
+++ src/views/VolumePanel.java
@@ -1,3 +1,12 @@
+/*
+* File: VolumePanel.java
+* Author: Gubis Zsombor Dániel
+* Copyright: 2022, Gubis Zsombor Dániel
+* Group: Szoft_II_N
+* Date: 2022-12-19
+* Github: https://github.com/Zsomi4/
+* Licenc: GNU GPL
+*/
 package views;
 
 public class VolumePanel extends InputPanel {
