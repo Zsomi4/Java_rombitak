c2d4366b1f72b17a67299e0771017da485b10592
Zsomi4 <k72302872172@ktch.hu>
2022-12-19 08:47:16 +0100

    initWindow és addComponents megírása.

--- src/views/MainView.java
+++ src/views/MainView.java
@@ -1,7 +1,27 @@
 package views;
 
+import javax.swing.BoxLayout;
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 
 public class MainView extends JFrame {
+
+    public MainView() {
+        
+    }
+
+    public void initWindow() {
+        setIconImage(new ImageIcon("logo.png").getImage());
+        setTitle("Rombitak");
+        setLayout(new BoxLayout(this.getContentPane(), BoxLayout.PAGE_AXIS));
+        addComponents();
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        pack();
+        setVisible(true);
+    }
+
+    public void addComponents() {
+        
+    }
     
 }
